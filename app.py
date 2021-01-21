c51647e15857ee6bde173ddb14b074d9b40da774
Aydin <aydinmisirzade@hotmail.com>
2021-01-21 13:39:00 +0100

    updated while loop

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 import requests
 import json
-import pandas as pd
+
 
 URL = "https://connection.eu-central-1.keboola.com/v2/storage/files"
 
 with open("config.json","r") as f:
     config = json.load(f)
 
-
 offset = 0
-params = {"showExpired":"true","limit":"1","offset":"0"}
-response = requests.get(url=URL,headers=config,params=params)
+limit = 100
+params = {"showExpired":"true","limit":"100","offset":"0"}
 
-with open("data.json","w") as f:
+response = requests.get(url=URL,headers=config,params=params)
+with open("response.csv","a") as f:
+    json_list = json.loads(response.text)
+    f.write(','.join(json_list[0].keys())+'\n')
+    for file in json_list:
+        f.write(','.join(str(x) for x in file.values())+'\n')
 
     while response.text != "[]":
         offset += 100
+        print("Current offset:",offset)
         params['offset'] = str(offset)
         response = requests.get(url=URL,headers=config,params=params)
-
-    params['offset'] = str(0)
-    params['limit'] = str(offset)
-    response = requests.get(url=URL,headers=config,params=params)
-    f.write(response.text)
-
-
-df = pd.read_json("data.json")
-df.to_csv("response.csv", index = None)
+        if response.text != "[]":
+            json_list = json.loads(response.text)
+            for file in json_list:
+                f.write(','.join(str(x) for x in file.values())+'\n')
